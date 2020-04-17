f2ccb140eb25cc8f9ff821fce5c7f5c12a3ec933
Oleksii Boiko <56755632+oleksii-boyko@users.noreply.github.com>
2020-04-17 20:44:25 +0300

    updated code for using local variables instead of global ones

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,32 +1,54 @@
 const config1 = ["https://loremflickr.com/320/240", "https://loremflickr.com/320/240/dog", "https://loremflickr.com/320/240/paris,girl/all"];
 const INITIAL_INDEX = 0;
-let currentImageIndex = INITIAL_INDEX;
-let prevButton, nextButton;
 
 putCarousel("carousel", config1);
 
 function putCarousel(id, images) {
+    let currentImageIndex = INITIAL_INDEX;
     let carousel = document.getElementById(id);
+    let imagesNum = images.length;
 
-    prevButton = document.createElement("i");
+    let prevButton = document.createElement("i");
     prevButton.className = "fas fa-chevron-left carousel-button left";
-    prevButton.addEventListener("click", function (){ previous(content)});
+    prevButton.addEventListener("click", function (){
+        previous(content);
+        currentImageIndex--;
+        currentImageIndex = checkButtons(prevButton, nextButton, currentImageIndex, imagesNum);
+    });
     carousel.appendChild(prevButton);
 
     let content = createCarouselMain(images, carousel);
 
-    nextButton = document.createElement("i");
+    let nextButton = document.createElement("i");
     nextButton.className = "fas fa-chevron-right carousel-button right";
-    nextButton.addEventListener("click", function (){ next(content)});
+    nextButton.addEventListener("click", function (){
+        next(content);
+        currentImageIndex++;
+        currentImageIndex = checkButtons(prevButton, nextButton, currentImageIndex, imagesNum);
+    });
     carousel.appendChild(nextButton);
+
+    checkButtons(prevButton, nextButton, currentImageIndex, imagesNum);
+    shiftImages();
 }
 
 function createCarouselMain(images, carousel) {
     let main = document.createElement("div");
     main.className = "carousel-main";
+    carousel.appendChild(main);
 
     let content = document.createElement("ul");
     content.className = "carousel-content";
+    main.appendChild(content);
+    addImages(images, content);
+
+    let first = content.childNodes[INITIAL_INDEX];
+    first.classList.add("current");
+
+    return content;
+}
+
+function addImages(images, content) {
     for (let i = 0; i < images.length; i++) {
         let carousel_element = document.createElement("li");
         carousel_element.className = "carousel-image";
@@ -35,38 +57,23 @@ function createCarouselMain(images, carousel) {
         carousel_element.appendChild(image);
         content.appendChild(carousel_element);
     }
-
-    let first = content.childNodes[INITIAL_INDEX];
-    first.classList.add("current");
-
-    main.appendChild(content);
-    carousel.appendChild(main);
-    return content;
 }
-
-let images = shiftImages();
-checkButtons();
 
 function shiftImages() {
     let images = document.getElementsByClassName("carousel-image");
     for (let i = 0; i < images.length; i++) {
         images[i].style.left = images[i].getBoundingClientRect().width * i + 'px';
     }
-    return images;
 }
 
 function next(content){
     let currentSlide = content.querySelector(".current");
     moveTo(content, currentSlide, currentSlide.nextElementSibling);
-    currentImageIndex++;
-    checkButtons();
 }
 
 function previous(content) {
     let currentSlide = content.querySelector(".current");
     moveTo(content, currentSlide, currentSlide.previousElementSibling);
-    currentImageIndex--;
-    checkButtons();
 }
 
 function moveTo(content, from, target) {
@@ -76,20 +83,21 @@ function moveTo(content, from, target) {
     target.classList.add("current");
 }
 
-function checkButtons() {
-    if (currentImageIndex >= images.length - 1){
-        nextButton.style.visibility = "hidden";
-        currentImageIndex = images.length - 1;
+function checkButtons(prev, next, index, max) {
+    if (index >= max - 1){
+        next.style.visibility = "hidden";
+        index = max - 1;
     }
     else {
-        nextButton.style.visibility = "visible";
+        next.style.visibility = "visible";
     }
 
-    if (currentImageIndex <= 0){
-        prevButton.style.visibility = "hidden";
-        currentImageIndex = 0;
+    if (index <= 0){
+        prev.style.visibility = "hidden";
+        index = 0;
     }
     else {
-        prevButton.style.visibility = "visible";
+        prev.style.visibility = "visible";
     }
+    return index;
 }
