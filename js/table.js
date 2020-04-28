481b7a46eb6eaa67e4385b9334d4ea35cac1b809
Oleksii Boiko <56755632+oleksii-boyko@users.noreply.github.com>
2020-04-28 15:24:31 +0300

    added sorting buttons and sorting itself

--- js/table.js
+++ js/table.js
@@ -37,6 +37,62 @@ function DataTable(config, data) {
         function createColumn(colElement) {
             const column = document.createElement("th");
             column.innerHTML = colElement["title"];
+
+            if (colElement["sortable"] === true){
+                let sortButton = document.createElement("button");
+                sortButton.classList.add("sort-button");
+                let icon = document.createElement("i");
+                icon.classList.add("fas");
+                icon.classList.add("fa-sort");
+                sortButton.appendChild(icon);
+                sortButton["state"] = 0;
+
+                sortButton.addEventListener("click", function () {
+                    let buttons = document.getElementsByClassName("sort-button");
+
+                    for (let i = 0; i < buttons.length; i++){
+                        discardButton(buttons[i]);
+                    }
+
+                    function discardButton(button){
+                        if (button !== sortButton){
+                            button["state"] = 0;
+                            setActualClass(button);
+                        }
+                    }
+
+                    sortButton["state"] += Math.PI / 2;
+                    setActualClass(sortButton);
+
+                    let body = (table.getElementsByTagName("tbody")[0]);
+                    table.removeChild(body);
+                    table.appendChild(createBody(config, sortArray(data, colElement["value"], sortButton["state"])));
+                });
+
+                function setActualClass(button) {
+                    let buttonClasses = button.firstChild.classList;
+                    for (let i = 0; i < buttonClasses.length; i++) {
+                        if (buttonClasses[i].includes("fa-")) {
+                            buttonClasses.remove(buttonClasses[i]);
+                            buttonClasses.add(obtainActualClass(button["state"]));
+                            break;
+                        }
+                    }
+                }
+
+                function obtainActualClass(state) {
+                    if (calculateIntSine(state) === -1){
+                        return "fa-sort-down"
+                    }
+                    else if (calculateIntSine(state) === 0){
+                        return "fa-sort"
+                    }
+                    else if (calculateIntSine(state) === 1){
+                        return "fa-sort-up"
+                    }
+                }
+                column.appendChild(sortButton);
+            }
             content.appendChild(column);
         }
         return head;
