34ef5aa550dfd6aa855f981ac2bf470c93b2c5cd
Oleksii Boiko <56755632+oleksii-boyko@users.noreply.github.com>
2020-04-28 15:22:59 +0300

    added table creation by initial data

--- js/table.js
+++ js/table.js
@@ -18,6 +18,55 @@ const users = [
     {id: 32222, name: 'Серьезно?', surname: 'Вася?', age: 14}
 ];
 
+DataTable(tableConfig, users);
+
+function DataTable(config, data) {
+    let parent = document.getElementById(config.parent);
+
+    const table = document.createElement("table");
+    table.appendChild(createHead());
+    table.appendChild(createBody(config, data));
+    parent.appendChild(table);
+
+    function createHead() {
+        const head = document.createElement("thead");
+        const content = document.createElement("tr");
+        head.appendChild(content);
+        config.columns.forEach(createColumn);
+
+        function createColumn(colElement) {
+            const column = document.createElement("th");
+            column.innerHTML = colElement["title"];
+            content.appendChild(column);
+        }
+        return head;
+    }
+
+    function createBody(config, data) {
+        const body = document.createElement("tbody");
+        let index = 1;
+        data.forEach(createRow);
+
+        function createRow(rowContent) {
+            rowContent["index"] = index;
+            const row = document.createElement("tr");
+            config.columns.forEach(createColumn);
+            body.appendChild(row);
+
+            function createColumn(colElement) {
+                const col = document.createElement("td");
+                col.innerHTML = rowContent[colElement.value];
+                if (colElement.type === "number"){
+                    col.classList.add("align-right");
+                }
+                row.appendChild(col);
+            }
+
+            index++;
+        }
+        return body;
+    }
+
     function sortArray(array, property, multiplier) {
         return (Array.from(array)).sort(function(a, b){return isNaN(Number(a[property])) ? calculateIntSine(multiplier) * (a[property].localeCompare(b[property])) : calculateIntSine(multiplier) * (a[property] - b[property])});
     }
