41927ac5c33e42268c177c7c094d3efc089b8a8b
Oleksii Boiko <56755632+oleksii-boyko@users.noreply.github.com>
2020-05-11 10:09:26 +0300

    added validation support

--- js/my-input.js
+++ js/my-input.js
@@ -23,16 +23,28 @@ Vue.component('my-input', {
             type: String,
             required: true
         },
+        },
     methods: {
+        validate: function () {
+            this["cl"] = !(this["required"] && !this["value"].match(this["pattern"]));
+        },
         getRequired: function (arg) {
             return arg ? "required" : "";
         },
+        setValid: function () {
+            this["cl"] = true;
+        }
+    },
+    data() {
+        return {
+            cl : true
+        };
     },
     template: `
 <div class="i-group">
     <label :for=id>{{title}}<span v-if="required">*</span></label>
-    <input :id=id :placeholder=placeholder
-    :type=type :required=getRequired(required)>
+    <input :id=id :placeholder=placeholder :class="{ valid : cl, invalid : !cl }"
+    :type=type :required=getRequired(required) v-on:input="setValid()" v-on:blur="validate()">
 </div>
 `
 });
