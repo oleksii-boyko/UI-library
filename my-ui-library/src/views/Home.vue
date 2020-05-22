2caa003c4e84496d59b46f91e13cad147753e5c8
Oleksii Boiko <56755632+oleksii-boyko@users.noreply.github.com>
2020-05-22 16:33:24 +0300

    added example for my-button

--- my-ui-library/src/views/Home.vue
+++ my-ui-library/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <MyButton size="lg" variant="warning" @click="printClick">Some text</MyButton>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +9,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import MyButton from '@/components/MyButton.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton
+  },
+  methods: {
+    printClick: function () {
+      console.log("QQ");
+    }
   }
 }
 </script>
